8c11731fe34949f4fbb5fdd00d7630616ca408e6
nkwaaaa <nkwaaaa@gmail.com>
2023-06-04 11:48:44 -0300

    add upload file

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,18 +8,26 @@ export default function Home() {
   return (
     <div>
       <form
-        onSubmit={(e) => {
+        onSubmit={async (e) => {
           e.preventDefault()
 
           if (!file) return
 
-          console.log(file)
+          const form = new FormData()
+          form.set("file", file)
+
+          const res = await fetch("/api/upload", {
+            method: "POST",
+            body: form,
+          })
+          const data = await res.json()
+          console.log(data)
         }}
       >
-        <label> Upload file</label>
-
-        <input type="file" onChange={(e) => setFile(e.target.files[0])} />
-
+        <label>
+          Upload file
+          <input type="file" onChange={(e) => setFile(e.target.files[0])} />
+        </label>
         <button>Upload</button>
       </form>
     </div>
